f0c70d6845faaf5bf95b9ccac1338f9f5e33dc75
fc-000 <142074177+fc-000@users.noreply.github.com>
2026-02-14 12:55:18 +0800

    added Functions folder

---
+++ Functions/script.js
@@ -0,0 +1,19 @@
+function add7(num) {
+  return num + 7;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function capitalize(word) {
+  return word.toUpperCase();
+}
+
+function lastLetter(word) {
+  return word[word.length - 1]
+}
+
+result = lastLetter("abCde");
+console.log(result);
+
