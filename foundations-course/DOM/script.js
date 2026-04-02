3719fe56df7d96fbfadfca8e25868b630e3e1aef
fc-000 <142074177+fc-000@users.noreply.github.com>
2026-04-02 16:08:39 +0800

    Added DOM and Events folder

---
+++ foundations-course/DOM/script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+const p = document.createElement("p");
+p.textContent = "Hey I'm red!";
+p.style.color = "red";
+
+const h3 = document.createElement("h3");
+h3.textContent = "I'm a blue h3!";
+h3.style.color = "blue";
+
+const div = document.createElement("div");
+div.style.border = "2px solid black";
+div.style.backgroundColor = "pink";
+
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
+
+const paragraph = document.createElement("p");
+paragraph.textContent = "ME TOO!";
+
+div.appendChild(h1);
+div.appendChild(paragraph);
+
+container.appendChild(div);
